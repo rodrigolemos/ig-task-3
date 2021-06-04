b2a0a97835ac936efe04d9ebfd0f20e55d583b72
Rodrigo <rodrigolemosrl@hotmail.com>
2021-06-04 19:26:41 -0300

    feat: first home element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,33 @@
-import { GetStaticProps } from 'next';
+import { ReactElement } from 'react';
+// import { GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
+
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
+
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+export default function Home(): ReactElement {
+  return <h1 className={styles.title}>Hello world!</h1>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
