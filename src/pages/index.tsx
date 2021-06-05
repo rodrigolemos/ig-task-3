2bfe7ae7f0e2ba7c38d28f2a256ef947f976cfdd
Rodrigo <rodrigolemosrl@hotmail.com>
2021-06-05 18:24:18 -0300

    feat: render static and dynamic posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,126 @@
-import { ReactElement } from 'react';
+import { useState, ReactElement } from 'react';
 import { FiCalendar, FiUser } from 'react-icons/fi';
-// import { GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-// import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-// interface Post {
-//   uid?: string;
-//   first_publication_date: string | null;
-//   data: {
-//     title: string;
-//     subtitle: string;
-//     author: string;
-//   };
-// }
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
 
-// interface PostPagination {
-//   next_page: string;
-//   results: Post[];
-// }
+interface PostPagination {
+  next_page: string;
+  results: Post[];
+}
 
-// interface HomeProps {
-//   postsPagination: PostPagination;
-// }
+interface HomeProps {
+  postsPagination: PostPagination;
+}
 
-export default function Home(): ReactElement {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handlePostResult = (posts: any): Post[] => {
+  return posts.map((post: Post) => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        { locale: ptBR }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+};
+
+export default function Home({ postsPagination }: HomeProps): ReactElement {
+  const { next_page, results } = postsPagination;
+  const [posts, setPosts] = useState<Post[]>([...results]);
+  const [hasNextPage, setHasNextPage] = useState<string | null>(next_page);
+
+  const fetchNextPage = async (): Promise<void> => {
+    try {
+      const response = await fetch(hasNextPage);
+      if (response.status === 200) {
+        const data = await response.json();
+
+        setPosts([...posts, ...handlePostResult(data.results)]);
+        setHasNextPage(data.next_page);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <main className={styles.container}>
         <div className={commonStyles.content}>
-          <article className={styles.post}>
-            <h1 className={styles.title}>
-              Lorem ipsum dolor sit amet consectetur
-            </h1>
-            <h2 className={styles.subtitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rerum
-              esse nemo cupiditate debitis alias
-            </h2>
-            <div className={styles.info}>
-              <div>
-                <FiCalendar />
-                <span>15 Mar 2021</span>
+          {posts.map(post => (
+            <article key={`post-${post.uid}`} className={styles.post}>
+              <h1 className={styles.title}>{post.data.title}</h1>
+              <h2 className={styles.subtitle}>{post.data.subtitle}</h2>
+              <div className={styles.info}>
+                <div>
+                  <FiCalendar />
+                  <span>{post.first_publication_date}</span>
+                </div>
+                <div>
+                  <FiUser />
+                  <span>{post.data.author}</span>
+                </div>
               </div>
-              <div>
-                <FiUser />
-                <span>Joseph Oliveira</span>
-              </div>
-            </div>
-          </article>
-          <article className={styles.post}>
-            <h1 className={styles.title}>
-              Lorem ipsum dolor sit amet consectetur
-            </h1>
-            <h2 className={styles.subtitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rerum
-              esse nemo cupiditate debitis alias
-            </h2>
-            <div className={styles.info}>
-              <div>
-                <FiCalendar />
-                <span>15 Mar 2021</span>
-              </div>
-              <div>
-                <FiUser />
-                <span>Joseph Oliveira</span>
-              </div>
-            </div>
-          </article>
+            </article>
+          ))}
+          {hasNextPage && (
+            <button
+              type="button"
+              className={styles.more}
+              onClick={fetchNextPage}
+            >
+              Carregar mais posts
+            </button>
+          )}
         </div>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 2,
+    }
+  );
 
-//   // TODO
-// };
+  const posts: Post[] = handlePostResult(postsResponse.results);
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      },
+    },
+    // revalidate: 3600,
+  };
+};
