6aaf99734e134ad4960b5713bd35f5a459d369b0
Rodrigo <rodrigolemosrl@hotmail.com>
2021-06-05 00:52:13 -0300

    feat: static post layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { ReactElement } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 // import { GetStaticProps } from 'next';
 
 // import { getPrismicClient } from '../services/prismic';
@@ -26,7 +28,53 @@ import styles from './home.module.scss';
 // }
 
 export default function Home(): ReactElement {
-  return <h1 className={styles.title}>Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <div className={styles.content}>
+          <article className={styles.post}>
+            <h1 className={styles.title}>
+              Lorem ipsum dolor sit amet consectetur
+            </h1>
+            <h2 className={styles.subtitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rerum
+              esse nemo cupiditate debitis alias
+            </h2>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar />
+                <span>15 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </article>
+          <article className={styles.post}>
+            <h1 className={styles.title}>
+              Lorem ipsum dolor sit amet consectetur
+            </h1>
+            <h2 className={styles.subtitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rerum
+              esse nemo cupiditate debitis alias
+            </h2>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar />
+                <span>15 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }
 
 // export const getStaticProps = async () => {
